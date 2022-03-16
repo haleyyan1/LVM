94eae62b772603b1ebcc020943e66b55ab61ded2
BT_1E10_20 <sample@bths.edu>
2022-03-16 12:27:05 -0400

    Logical

--- src/Vg.java
+++ src/Vg.java
@@ -2,7 +2,11 @@ import java.util.ArrayList;
 public class Vg {
     private String na;
     private ArrayList<Pv> pv;
-    public Vg(String nam){
+    public Vg(String nam,Pv p){
         na=nam;
+        pv.add(p);
+    }
+    public void extend(Pv p){
+        pv.add(p);
     }
 }
